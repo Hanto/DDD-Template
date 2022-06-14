9cfb1203044cce16d87a97a30f7607a187337ee5
jhanto <jhanto@gmail.com>
2022-06-14 07:33:42 +0200

    work in progress

--- src/main/java/com/ddd/context/domain/model/DomainAggregate.java
+++ src/main/java/com/ddd/context/domain/model/DomainAggregate.java
@@ -1,31 +1,5 @@
 package com.ddd.context.domain.model;// Created by jhant on 11/06/2022.
 
-import com.ddd.context.domain.events.DomainEvent;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public abstract class DomainAggregate extends DomainEntity
 {
-    public abstract List<DomainEvent>pullAllDomainEvents();
-
-    // HELPER:
-    //--------------------------------------------------------------------------------------------------------
-
-    @SafeVarargs
-    protected final <T extends DomainEntity> void pullEventsFrom(T... entities)
-    {   pullEventsFrom(Arrays.asList(entities)); }
-
-    protected <T extends DomainEntity>void pullEventsFrom(Collection<T> entities)
-    {
-        Collection<DomainEvent> pulledEvents = entities.stream()
-            .map(domainEntity -> domainEntity instanceof DomainAggregate ?
-                ((DomainAggregate) domainEntity).pullAllDomainEvents() :
-                domainEntity.pullDomainEvents())
-            .flatMap(Collection::stream).collect(Collectors.toList());
-
-        recordEvents(pulledEvents);
-    }
 }
--- src/main/java/com/ddd/context/domain/model/DomainEntity.java
+++ src/main/java/com/ddd/context/domain/model/DomainEntity.java
@@ -3,7 +3,6 @@ package com.ddd.context.domain.model;// Created by jhant on 10/06/2022.
 import com.ddd.context.domain.events.DomainEvent;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,16 +13,13 @@ public abstract class DomainEntity
     // MAIN:
     //--------------------------------------------------------------------------------------------------------
 
-    final public List<DomainEvent>pullDomainEvents()
+    final protected void recordEvent(DomainEvent event)
+    {   domainEvents.add(event); }
+
+    final public List<DomainEvent> pullEvents()
     {
         List<DomainEvent>events = domainEvents;
         domainEvents = Collections.emptyList();
         return events;
     }
-
-    final protected void recordEvent(DomainEvent event)
-    {   domainEvents.add(event); }
-
-    final protected void recordEvents(Collection<DomainEvent> events)
-    {   domainEvents.addAll(events); }
 }
