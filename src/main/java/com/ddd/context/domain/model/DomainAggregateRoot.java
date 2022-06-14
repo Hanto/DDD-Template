b1c5df43c10d6c02a06f7669abbecc1dc3f42d2f
jhanto <jhanto@gmail.com>
2022-06-14 08:16:22 +0200

    work in progress

--- src/main/java/com/ddd/context/domain/model/DomainAggregateRoot.java
+++ src/main/java/com/ddd/context/domain/model/DomainAggregateRoot.java
@@ -1,5 +1,25 @@
 package com.ddd.context.domain.model;// Created by jhant on 11/06/2022.
 
+import com.ddd.context.domain.events.DomainEvent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class DomainAggregateRoot extends DomainAggregate
 {
+    private List<DomainEvent> domainEvents = new ArrayList<>();
+
+    // MAIN:
+    //--------------------------------------------------------------------------------------------------------
+
+    final protected void recordEvent(DomainEvent event)
+    {   domainEvents.add(event); }
+
+    final public List<DomainEvent> pullEvents()
+    {
+        List<DomainEvent>events = domainEvents;
+        domainEvents = Collections.emptyList();
+        return events;
+    }
 }
--- src/main/java/com/ddd/context/domain/model/DomainEntity.java
+++ src/main/java/com/ddd/context/domain/model/DomainEntity.java
@@ -1,25 +1,5 @@
 package com.ddd.context.domain.model;// Created by jhant on 10/06/2022.
 
-import com.ddd.context.domain.events.DomainEvent;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public abstract class DomainEntity
 {
-    private List<DomainEvent>domainEvents = new ArrayList<>();
-
-    // MAIN:
-    //--------------------------------------------------------------------------------------------------------
-
-    final protected void recordEvent(DomainEvent event)
-    {   domainEvents.add(event); }
-
-    final public List<DomainEvent> pullEvents()
-    {
-        List<DomainEvent>events = domainEvents;
-        domainEvents = Collections.emptyList();
-        return events;
-    }
 }
