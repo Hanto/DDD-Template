d31c4c500c23f83594d9ba6006d1913c5b1b81bf
jhanto <jhanto@gmail.com>
2022-06-15 14:55:12 +0200

    work in progress

--- src/main/java/com/ddd/context/domain/repositories/OutboxRepository.java
+++ src/main/java/com/ddd/context/domain/repositories/OutboxRepository.java
@@ -1,6 +1,6 @@
 package com.ddd.context.domain.repositories;// Created by jhant on 10/06/2022.
 
-import com.ddd.context.domain.out.Event;
+import com.ddd.context.application.ports.Event;
 
 public interface OutboxRepository
 {
